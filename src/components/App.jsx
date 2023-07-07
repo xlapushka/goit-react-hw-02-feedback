408e42ff386c3a4c9753239780e304fc8c26cb30
Yuliia Hubarenko <xlapushka@ukr.net>
2023-07-07 14:04:19 +0300

    initiate page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <p>Hello World and lets dance!</p>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
